5aa655c56d63ee493a580d695638def594ae42eb
Wyatt Stephenson <wyattstephenson@rocketmail.com>
2023-09-10 18:59:28 -0600

    working part of speech function, but it stops receving data due to too many queries to the API

--- src/MadLib.jsx
+++ src/MadLib.jsx
@@ -1,27 +1,30 @@
 import React from "react"
-import {useEffect} from "react"
-import {useContext, createContext, useState} from "react"
+import {useState, useEffect, useContext, createContext } from "react"
 import {MadLibContext } from "./App"
 
 
 function MadLib () {
 
-    function determinePartOfSpeech(word) {
-        let part = []
-        useEffect(()=>{
-            fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    function determinePartOfSpeech(partOfSpeechArray) {
+        let tempArray = []
+        for(let i = 0; i < partOfSpeechArray.length; i++ ) {
+            fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${partOfSpeechArray[i]}`)
             .then((r)=>r.json())
             .then((data)=>{
-                setPartOfSpeech(data[0].meanings[0].partOfSpeech);
+                // setPartOfSpeech(data[0].meanings[0].partOfSpeech);
+                tempArray.push(data[0].meanings[0].partOfSpeech)
+                console.log("This is the tempArray inside the function: " + tempArray)
+                setPartOfSpeechArray([...tempArray])
+                console.log("This is the partOfSpeechArray in the function :" + partOfSpeechArray)
             }
             )
-            .catch((error)=>{return console.log("Error fetching dictionary API: " + error)}
-            ),[word]})
-    
+            .catch((error)=>{return console.log("Error fetching dictionary API: " + error)})
+        }
         
     }
     const {randomHero, madLibString} = useContext(MadLibContext)
-    const [statePartOfSpeech, setPartOfSpeech] = useState("")       
+    //First, this will be set to have the same elements as the words in the hero biography, then it will be changed to contain the corresponding part of speech of each word instead.
+    const [partOfSpeechArray, setPartOfSpeechArray] = useState([])   
 
         //Removes a word at random, inserts user input field.
         function createMadLib (tempString) {
@@ -33,23 +36,20 @@ function MadLib () {
             headerBody.appendChild(subBody)
             let finalArray = []
 
+            //Copy the current hero bio array for use to determine what part of speech each element in the array is.
+            setPartOfSpeechArray([...newArray])
+
             //Randomly replace a word in the hero bio with "keyWord," if the word is a noun, verb, adverb, or adjective.
             for(let i = 0; i < (newArray.length) / 15; i++){
                 //set random index for newArray
                 let randomI = Math.floor(Math.random() * newArray.length)
-
-                // determinePartOfSpeech(newArray[i])
-                //     if (statePartOfSpeech === "noun" || statePartOfSpeech ===  "verb" || statePartOfSpeech === "adverb" || statePartOfSpeech === "adjective" ) {
-                            newArray.splice(randomI, 1, keyWord)
-                    // }
-
-
+                newArray.splice(randomI, 1, keyWord)
                 //Reset random index.
                 randomI = 0
             }
 
             let newString = newArray.join(" ")
-
+           
             for(let i = 0; i < newArray.length; i++) {
                 if(newArray[i] === "keyWord") {
                     const newInput = document.createElement("input")
@@ -70,14 +70,17 @@ function MadLib () {
             finalArray = newArray.slice()
         }
 
+
 //Call the create MadLib function to initialize the Mad Lib String.  Log current Hero loaded.
 useEffect (()=>{
-    if(randomHero){
-    
-      createMadLib(randomHero.bio)
-    
-    }
+    randomHero ? createMadLib(randomHero.bio) : null
     }, [randomHero])
+
+    console.log("This is the partOfSpeechArray: " + partOfSpeechArray)
+    useEffect(()=>{
+        determinePartOfSpeech(partOfSpeechArray)
+    },[])
+
 
 return(
     <>
