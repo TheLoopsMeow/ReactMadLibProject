8a44090f65acf097a46c3fefcbc9e514dc55ec78
Wyatt Stephenson <wyattstephenson@rocketmail.com>
2023-09-11 10:09:53 -0600

    flashing neon title

--- src/Header.jsx
+++ src/Header.jsx
@@ -9,13 +9,22 @@ import { MadLibContext } from "./App"
 function Header () {
     //Part 2 of importing the Context
     const {randomHero, madLibString} = useContext(MadLibContext)
-  
-   
+    const [flash, setFlash] = useState("title")
+
+   setInterval(()=>{
+    if(flash === "title"){
+        setFlash("titleFlash")
+    }
+    else {
+        setFlash("title")
+    }
+   }, 2000)
 return(
 
 <>
 {randomHero ? (
     <>
+    <span id="title">S</span><span id={flash}>u</span><span id="title">pe</span><span id={flash}>r</span><span id="title">hero</span><span id={flash}>Mad</span><span id="title"> Libs</span>
     <h1 id="name">{randomHero.name}</h1>
     <img src={randomHero.image}></img>
     </>
